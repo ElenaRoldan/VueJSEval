56b158e108cf05eb911a8fe6524d87c407c68959
Héléna ROLDAN <helena.roldan@aidalinfo.fr>
2025-06-06 14:11:59 +0200

    New project front

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <HeaderApp />
+    <main>
+        <RouterView/>
+    </main>
+    <FooterApp />
+</template>
+<script setup>
+import FooterApp from './components/FooterApp.vue';
+import HeaderApp from './components/HeaderApp.vue';
+
+
+</script>
